ec898396c267df4fa403c397642c7246c2869178
Reuben <4957964+reubn@users.noreply.github.com>
2024-02-25 17:59:10 +0000

    Remove SQLite

--- network/GraphQL.swift
+++ network/GraphQL.swift
@@ -1,26 +1,12 @@
 import Foundation
 
 import Apollo
-//import ApolloSQLite
 
 class GraphQL {
   let remoteURL = URL(string: "https://countries.trevorblades.com/")!
-  let sqliteDBFolder = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: .documentsDirectory, create: true)
-
-  lazy var sqliteDBFile = sqliteDBFolder?.appendingPathComponent("apolloBugApp.sqlite")
 
   lazy var apollo: ApolloClient = {
-    guard let sqliteDBFile = self.sqliteDBFile,
-          let sqliteCache = try? SQLiteNormalizedCache(fileURL: sqliteDBFile)
-    else {
-      print("FAILED TO CREATE CACHING APOLLO")
-      return ApolloClient(url: remoteURL)
-    }
-
-    let store = ApolloStore(cache: sqliteCache)
-    let normalTransport = RequestChainNetworkTransport(interceptorProvider: DefaultInterceptorProvider(store: store), endpointURL: remoteURL)
-
-    return ApolloClient(networkTransport: normalTransport, store: store)
+    return ApolloClient(url: remoteURL)
   }()
 
   static let shared = GraphQL()
